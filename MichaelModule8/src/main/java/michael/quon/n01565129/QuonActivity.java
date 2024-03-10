67bb9df3d1db7af95c97889b4c91abd7e96eb157
Michael-Quon <michaelquon08@gmail.com>
2024-03-10 19:16:44 -0400

    Created the AlertDialog to pop up when the back key is pressed when the sliding window is close, and is not closeable unless you either press yes or no

--- MichaelModule8/src/main/java/michael/quon/n01565129/QuonActivity.java
+++ MichaelModule8/src/main/java/michael/quon/n01565129/QuonActivity.java
@@ -3,10 +3,13 @@ package michael.quon.n01565129;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -54,16 +57,31 @@ public class QuonActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START);
-            } else {
-                super.onOptionsItemSelected(item);
-            }
-            return true;
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            // If the drawer is closed, display an AlertDialog
+            new AlertDialog.Builder(this)
+                    .setIcon(R.drawable.humberlogobg) // Set a custom icon for the AlertDialog
+                    .setTitle(getString(R.string.name)) // Set the title of the AlertDialog
+                    .setMessage(R.string.exit) // Set the message of the AlertDialog
+                    .setCancelable(false) // User must not be able to dismiss the dialog without answering Yes or No
+                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // If Yes button is clicked, exit the app
+                            finish();
+                        }
+                    })
+                    .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // If No button is clicked, dismiss the dialog
+                            dialog.dismiss();
+                        }
+                    })
+                    .show(); // Show the AlertDialog
         }
-        return super.onOptionsItemSelected(item);
     }
-
 }
