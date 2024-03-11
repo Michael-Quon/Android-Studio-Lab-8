f4e580ee2f344982c5aff7cb16b75a545c1bb459
Michael-Quon <michaelquon08@gmail.com>
2024-03-10 23:30:46 -0400

    Edited the home fragment to now have my name in red text, an image view, and a button when pressed will show a snackbar and rotate between 4 images.

--- MichaelModule8/src/main/java/michael/quon/n01565129/HomeFragment.java
+++ MichaelModule8/src/main/java/michael/quon/n01565129/HomeFragment.java
@@ -8,6 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeFragment extends Fragment {
 
@@ -15,15 +20,36 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private ImageView imageView;
+    private final int[] imageResource = {R.drawable.bulbasaur, R.drawable.charmander, R.drawable.squritle, R.drawable.pikachu};
+    private int imageIndex = 0;
+    private int clicks = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        // initialize elements
+        TextView textView = view.findViewById(R.id.Mic_textView);
+        Button button = view.findViewById(R.id.Mic_button);
+        imageView = view.findViewById(R.id.Mic_imageView);
+
+        // set default image
+        imageView.setImageResource(imageResource[imageIndex]);
+
+        // set click listner for button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // change image on click
+                imageIndex = (imageIndex + 1) % imageResource.length;
+                imageView.setImageResource(imageResource[imageIndex]);
+
+                // snackbar
+                Snackbar.make(view, getString(R.string.name_id_clicks) + clicks++, Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
